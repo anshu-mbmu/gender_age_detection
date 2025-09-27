6668ea20e78b0e0dc4ba99bfc547664a3ef90429
anshu-mbmu <anshumaliparashar@gmail.com>
2025-09-27 14:30:43 +0530

    Update model.py

--- Models/model.py
+++ Models/model.py
@@ -1,5 +1,5 @@
 #test model
 #age_gender_model_keras
-https://drive.google.com/file/d/1MQyNO8lF8yMaCBt6tAswmikfzt-x4wFC/view?usp=sharing
+# https://drive.google.com/file/d/1MQyNO8lF8yMaCBt6tAswmikfzt-x4wFC/view?usp=sharing
 #age_gender_model_h5
-https://drive.google.com/file/d/1WS679mHugm-yhxVt-fhZv-KvZlOCyKPT/view?usp=drive_link
+# https://drive.google.com/file/d/1WS679mHugm-yhxVt-fhZv-KvZlOCyKPT/view?usp=drive_link
